7531edac0b7c82c34618d1efd63b6be5e414cd79
Jayantimishra <mishrajayanti956@gmail.com>
2025-11-22 15:42:42 +0530

    commited

---
+++ Express.js/day-1/data/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    name: "John",
+    displayname: "John Doe",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    displayname: "Jane Smith",
+  },
+  {
+    id: 3,
+    name: "Alice",
+    displayname: "Alice Johnson",
+  },
+  {
+    id: 4,
+    name: "Bob",
+    displayname: "Bob Brown",
+  },
+  { id: 5, 
+    name: "Charlie", 
+    displayname: "Charlie Davis" 
+},
+];
+
+
+export default  data ;
--- Express.js/day-1/index.js
+++ Express.js/day-1/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import Userdata from "./data/data.js"
+
 const app = express();
 const PORT = 3000;
+
+// * 1. Get Request (it is used to fetch data from the server)
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Hello World from Express.js');
+});
+
+// Industry Standard 
+
+app.get('/api/v1/users', (req, res) => {
+    res.status(200);
+    res.send( Userdata );
+});
 
 
 app.listen(PORT, (req, res) => {
